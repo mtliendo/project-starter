a2c2cdee63e10790b708a957cf6d1811623496ee
mtliendo <mtliendo@focusotter.com>
2025-03-07 11:38:07 -0600

    Configure DaisyUI themes and update theme controller

--- src/components/ThemeController.tsx
+++ src/components/ThemeController.tsx
@@ -2,7 +2,7 @@ function ThemeController() {
 	return (
 		<label className="swap swap-rotate">
 			{/* this hidden checkbox controls the state */}
-			<input type="checkbox" className="theme-controller" value="dark" />
+			<input type="checkbox" className="theme-controller" value="night" />
 
 			{/* sun icon */}
 			<svg
